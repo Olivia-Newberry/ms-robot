33bfea891903aec57578595c0032440c61b3f01f
Olivia-Newberry <55136903+Olivia-Newberry@users.noreply.github.com>
2021-11-22 10:34:19 +0000

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -23,7 +23,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ["update"],
   permLevel: "Bot Admin"
 };
 
