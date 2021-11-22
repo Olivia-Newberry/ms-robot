764a8095b8fc749c3e9accc32939f82f79a86349
Olivia-Newberry <55136903+Olivia-Newberry@users.noreply.github.com>
2021-11-22 09:55:33 +0000

    Create dog.js

---
+++ commands/dog.js
@@ -0,0 +1,40 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  let animal = 'dog';
+  const { link } = await fetch('https://some-random-api.ml/animal/'+animal).then(response => response.json());
+  //returns a random dog image and fact with format:
+  //{
+    // "image": "some link",
+    // "fact": "some fact"
+  //}
+  //get image
+  let img = link['image'];
+  let fact = link['fact'];
+  
+  const embed = {
+    description: "Random "+animal+" picture:",
+    title: fact,
+    image: {
+      url: img
+    },
+    //random color
+    color: Math.floor(Math.random() * 16777215).toString(16)
+  };
+  
+  message.channel.send({ embeds: [embed] });
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "dog",
+  category: "Fun",
+  description: "Grab a random dog fact and cute picture",
+  usage: "dog"
+};
