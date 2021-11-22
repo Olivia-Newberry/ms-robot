8bce8f85700433651baf801fe255e5e694a77d19
Olivia-Newberry <55136903+Olivia-Newberry@users.noreply.github.com>
2021-11-22 10:03:46 +0000

    more commands

---
+++ commands/birb.js
@@ -0,0 +1,38 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  let animal = 'birb';
+  const { image, fact } = await fetch('https://some-random-api.ml/animal/'+animal).then(response => response.json());
+  //returns a random birb image and fact with format:
+  //{
+    // "image": "some link",
+    // "fact": "some fact"
+  //}
+  //get image
+  
+  const embed = {
+    description: "Random "+animal+" picture:",
+    title: fact,
+    image: {
+      url: image
+    },
+    //random color
+    color: Math.floor(Math.random() * 16777215).toString(16)
+  };
+  
+  message.channel.send({ embeds: [embed] });
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "birb",
+  category: "Fun",
+  description: "Grab a random birb fact and cute picture",
+  usage: "birb"
+};
