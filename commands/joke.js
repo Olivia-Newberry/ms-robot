c95a3050e54cb0dc808ad350b6ed3656ea6c06a6
Olivia-Newberry <55136903+Olivia-Newberry@users.noreply.github.com>
2021-11-23 16:13:01 +0000

    Create joke.js

---
+++ commands/joke.js
@@ -0,0 +1,32 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  
+  const { joke } = await fetch('https://some-random-api.ml/joke').then(response => response.json());
+  
+  const embed = {
+    title: joke,
+    //description: joke,
+    color: Math.floor(Math.random() * 16777215).toString(16),
+    footer: {
+      text: 'Initiated by '+message.author.username+'#'+message.author.discriminator,
+      icon_url: message.author.displayAvatarURL()
+    }
+  };
+  
+  message.channel.send({ embeds: [embed] });
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["funny"],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "joke",
+  category: "Fun",
+  description: "Tells a bad joke",
+  usage: "joke"
+};
