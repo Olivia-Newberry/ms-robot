68ed48cfd5d91d22966df13f7d15dc565c79c6f7
Olivia-Newberry <55136903+Olivia-Newberry@users.noreply.github.com>
2021-11-23 14:26:29 +0000

    Update triggered.js

--- commands/triggered.js
+++ commands/triggered.js
@@ -8,7 +8,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
   var avatar = user.displayAvatarURL()
   var triggeredAvatar = ('https://some-random-api.ml/canvas/triggered?avatar='+avatar) 
   //replace webp with png
-  triggeredAvatar = triggeredAvatar.replace(".webp", ".png&file=image.gif")
+  triggeredAvatar = triggeredAvatar.replace(".webp", ".png&file=image.webp")
 
     const embeduserinfo = new MessageEmbed()
       .setColor("RANDOM")
