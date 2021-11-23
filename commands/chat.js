c6b59715d6f8582da8b32739138b1fd7904d4c55
Olivia-Newberry <55136903+Olivia-Newberry@users.noreply.github.com>
2021-11-23 18:04:13 +0000

    Create chat.js

---
+++ commands/chat.js
@@ -0,0 +1,26 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+  //combine the args into a string
+  let msg = args.join(" ");
+  //uri encode the message
+  msg = encodeURIComponent(msg);
+  //grab key from env
+  const key = process.env.SRA_KEY;
+  const { response } = await fetch('https://some-random-api.ml/chatbot?key='+key+'&message='+msg).then(response => response.json());
+  return await message.reply(response);
+  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['c'],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "chat",
+  category: "Miscellaneous",
+  description: "Chat with a bot",
+  usage: "chat"
+};
