7dc8ed5ba115ac3e36bf310b5ebb448637416923
Olivia-Newberry <55136903+Olivia-Newberry@users.noreply.github.com>
2021-11-23 16:14:58 +0000

    Create meme.js

---
+++ commands/meme.js
@@ -0,0 +1,35 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  
+  const { image, caption, category } = await fetch('https://some-random-api.ml/joke').then(response => response.json());
+  
+  const embed = {
+    title: caption,
+    description: category,
+    color: Math.floor(Math.random() * 16777215).toString(16),
+    image: {
+      url: image
+    },
+    footer: {
+      text: 'Initiated by '+message.author.username+'#'+message.author.discriminator,
+      icon_url: message.author.displayAvatarURL()
+    }
+  };
+  
+  message.channel.send({ embeds: [embed] });
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["mem"],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "meme",
+  category: "Fun",
+  description: "Shares a terrible meme",
+  usage: "meme"
+};
