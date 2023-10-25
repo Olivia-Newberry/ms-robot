bf9f4f5de37d73990ed9400e5186f706acb545c8
Olivia Newberry <55136903+Olivia-Newberry@users.noreply.github.com>
2023-10-25 15:04:49 +0000

    code quality updates

--- commands/botInfo.js
+++ commands/botInfo.js
@@ -4,40 +4,40 @@ const { DurationFormatter } = require("@sapphire/time-utilities");
 const durationFormatter = new DurationFormatter();
 
 exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const duration = durationFormatter.format(client.uptime);
-  const msg = {
-    title: "about ms robot",
-    description: `ms robot is a bot that is managed and hosted by @Cyndergosa#2493\nIf you wish to suggest changes, send me a DM or open a pull request over at github\n`,
-    fields: [
-      {
-        name: "Github",
-        value: `[Link](https://github.com/Olivia-Newberry/ms-robot/)`,
-        inline: true
-      },
-      {
-        name: "Discord Invite",
-        value: `[Link](https://discord.com/api/oauth2/authorize?client_id=719912462631305217&permissions=8&scope=applications.commands%20bot)`,
-        inline: true
-      },
-    ],
-    footer: {
-      text: "ms robot"
-    }
+    const duration = durationFormatter.format(client.uptime);
+    const msg = {
+        title: "about ms robot",
+        description: "ms robot is a bot that is managed and hosted by @Cyndergosa#2493\nIf you wish to suggest changes, send me a DM or open a pull request over at github\n",
+        fields: [
+            {
+                name: "Github",
+                value: "[Link](https://github.com/Olivia-Newberry/ms-robot/)",
+                inline: true
+            },
+            {
+                name: "Discord Invite",
+                value: "[Link](https://discord.com/api/oauth2/authorize?client_id=719912462631305217&permissions=8&scope=applications.commands%20bot)",
+                inline: true
+            },
+        ],
+        footer: {
+            text: "ms robot"
+        }
 
-  }
-  message.channel.send({ embeds: [msg]});
+    }
+    message.channel.send({ embeds: [msg] });
 };
 
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: "User"
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: "User"
 };
 
 exports.help = {
-  name: "botinfo",
-  category: "Miscellaneous",
-  description: "github and server invite links",
-  usage: "botinfo"
+    name: "botinfo",
+    category: "Miscellaneous",
+    description: "github and server invite links",
+    usage: "botinfo"
 };
