cb81ccd5c7acc2e4e1cd7c97899059f779d3fb68
Olivia Newberry <55136903+Olivia-Newberry@users.noreply.github.com>
2023-12-07 17:10:29 +0000

    Update partialMessage

--- commands/partialMessage.js
+++ commands/partialMessage.js
@@ -1,45 +1,45 @@
 exports.run = async (client, message, args, level, command) => { // eslint-disable-line no-unused-vars
-	//var command = args.shift().toLowerCase();
-	if (message.content.toLowerCase() === "ms robot test") {
-		message.reply('I\'m listening!');
-	}
-	if (message.content.toLowerCase() == "hi ms robot") {
-		message.reply('Hello, how are you today?');
-	}
-	if (message.content.toLowerCase().includes('minecraft')) {
-		message.react("634294300976349194")
-	}
+  var msgLower = message.content.toLowerCase();
 
-	if (
-		(
-			message.content.toLowerCase().includes('!verification')
-			|| message.content.toLowerCase().includes('! verification')
-		)
-		&& message.channel.name != undefined
-		&& !(['mossy','newcomers'].includes(message.channel.name.toLowerCase()))
-	) {
-		message.channel.send('!!verify');
-	}
+  if (["ms robot test", "anyone listening?"].includes(msgLower)) {
+    message.reply('I\'m listening!');
+  }
+  if (msgLower == "hi ms robot") {
+    message.reply('Hello, how are you today?');
+  }
+  if (msgLower.includes('minecraft')) {
+    message.react("634294300976349194")
+  }
 
-	//if channel name contains "selfie"
-	if (message.channel.name != undefined && message.channel.name.toLowerCase().includes("selfie")) {
-		if (message.attachments.size > 0) {
-			message.react("758296751861268540");
-		}
-	};
+  if (
+    msgLower.includes('!verification')
+    || msgLower.includes('! verification')
+  ){
+    if (message.channel.name != undefined && !(['mossy','newcomers'].includes(message.channel.name.toLowerCase()))) {
+      message.channel.send('!!verify');
+    } else {
+      message.reply('Please read the sticky message or try messaging Sora');
+    }
+  }
 
-}
+  //if channel name contains "selfie"
+  if (message.channel.name != undefined && message.channel.name.toLowerCase().includes("selfie")) {
+    if (message.attachments.size > 0) {
+      message.react("758296751861268540");
+    }
+  }
+};
 
 exports.conf = {
-	enabled: true,
-	guildOnly: false,
-	aliases: [],
-	permLevel: "User"
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
 };
 
 exports.help = {
-	name: "partialMessage",
-	category: "Miscellaneous",
-	description: "Easter eggs, funky things and others",
-	usage: "Just chat, you may notice it."
+  name: "partialMessage",
+  category: "Miscellaneous",
+  description: "Easter eggs, funky things and others",
+  usage: "Just chat, you may notice it."
 };
